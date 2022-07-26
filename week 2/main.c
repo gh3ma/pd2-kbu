5ad3de8d7d92ef33d11a1bbbecc4de3a12824a04
ghaıth almadanı <gaes.sy.2000@gmail.com>
2022-07-26 18:27:29 +0300

    second week's homework

---
+++ week 2/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ /* write a recursive function that finds the power of the  given number .*/
+ int power(int x, int y){
+    if (y != 1 ){
+        return  x* power(x , y - 1);
+    } else{
+        return x ;
+    }
+
+}
+
+ int main(){
+ int x , y ;
+     printf("enter your number: ");
+     scanf("%d" , &x);
+     printf("enter your power of number: ");
+     scanf("%d" , &y);
+     printf("result is %d " , power(x, y ));
+      return 0;
+
+ }
